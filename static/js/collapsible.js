edc354a44a07ace3f856b2262130b40ea7f27ae0
George Song <george@monozuku.com>
2020-01-18 13:07:05 -0800

    Update files to pass pre-commit check
    * Black format Python
    * Prettier format everything else
    * Correct HTML markup mistakes
    
    Address #175

--- static/js/collapsible.js
+++ static/js/collapsible.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-
-  $('.collapsible').click(function() {
-      $(this).toggleClass("collapsible-active");
-      $(this).next(".collapsible-content").toggle();
+  $(".collapsible").click(function() {
+    $(this).toggleClass("collapsible-active");
+    $(this)
+      .next(".collapsible-content")
+      .toggle();
   });
 
   /* Expanding a collapsible lengthens the page. If you expand one, navigate 
@@ -10,8 +11,8 @@ $(document).ready(function() {
   scroll to the bottom of the page. I could fix this by remembering collapsible
   state in a cookie or something, but the effort/reward ratio is poor. 
   Instead, just scroll back to the top. */
-  if ($('.collapsible')[0]) {
-    $(window).on('beforeunload', function(){
+  if ($(".collapsible")[0]) {
+    $(window).on("beforeunload", function() {
       $(window).scrollTop(0);
     });
   }
